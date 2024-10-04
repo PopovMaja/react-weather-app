09b18ea247130b108561c0b33d58afe7a3304fde
PopovMaja <167343423+PopovMaja@users.noreply.github.com>
2024-10-04 12:14:26 -0400

    Added GitHub and Netlify links to project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,29 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This website was created by{" "}
+        <a href="https://github.com/PopovMaja" target="_blank" rel="noreferrer">
+          Maja Popov
+        </a>{" "}
+        is it open-sorced on{" "}
+        <a
+          href="https://github.com/PopovMaja/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://reactappforweatherforecast.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+        .
+      </footer>
     </div>
   );
 }
