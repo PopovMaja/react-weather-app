10abfe0d88906c63f62e0685a8b6546fdb7405aa
PopovMaja <167343423+PopovMaja@users.noreply.github.com>
2024-10-04 12:20:48 -0400

    Added Bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -3,30 +3,36 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This website was created by{" "}
-        <a href="https://github.com/PopovMaja" target="_blank" rel="noreferrer">
-          Maja Popov
-        </a>{" "}
-        is it open-sorced on{" "}
-        <a
-          href="https://github.com/PopovMaja/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://reactappforweatherforecast.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This website was created by{" "}
+          <a
+            href="https://github.com/PopovMaja"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Maja Popov
+          </a>{" "}
+          is it open-sorced on{" "}
+          <a
+            href="https://github.com/PopovMaja/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://reactappforweatherforecast.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
