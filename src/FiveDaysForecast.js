f4851fccb953cd1789d6aa6ec63639550281971d
PopovMaja <167343423+PopovMaja@users.noreply.github.com>
2024-10-13 23:48:38 -0400

    Added media css for app

--- src/FiveDaysForecast.js
+++ src/FiveDaysForecast.js
@@ -23,7 +23,7 @@ export default function FiveDaysForecast(props) {
           {forecast.map(function (dailyForecast, index) {
             if (index < 5) {
               return (
-                <div className="col" key={index}>
+                <div className="col PerDay" key={index}>
                   <ForecastPerDay data={dailyForecast} />
                 </div>
               );
